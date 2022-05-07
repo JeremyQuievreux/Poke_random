0c5db18d22e1846fcc0d6ab766da03ba3e6ebb76
DevHÿllen <jquievreux.devpro@gmail.com>
2022-05-07 09:42:26 +0200

    start navbar

--- comps/Header.tsx
+++ comps/Header.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import styles from '../styles/comps/Header.module.scss'
 
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
+    <div className={styles.navbar_container}>
+      <div className={styles.navbar_logo}>
+        <Image src="/../public/pokeball2.png" width={40} height={40} />
+        <h1>Poké Random</h1>
+      </div>
+      <nav>
+        <Link href={"/"}><a>Accueil</a></Link>
+        <Link href={"/"}><a>Shop</a></Link>
+        <Link href={"/"}><a>Créer Compte</a></Link>
+        <Link href={"/"}><a>Se Connecter</a></Link>
+      </nav>
+    </div>
   )
 }
 
