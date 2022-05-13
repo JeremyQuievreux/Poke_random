5549154931e5bf4d4440e9e22db1d1291ee4e4f7
Quievreux <jquievreux.devpro@gmail.com>
2022-05-13 14:40:27 +0200

    selectorlist -> .ts

--- utils/selectorLists.js
+++
@@ -1,5 +0,0 @@
-export const typesPKM =["Acier","Combat", "Dragon", "Eau", "Electrik", "Fée", "Feu", "Glace", "Insecte", "Normal", "Plante", "Poison", "Psy", "Roche", "Sol", "Spectre", "Ténébre", "Vol"]
-
-export const rarityList = ["Commune", "Peu Commune", "Rare", "Légendaire"]
-
-export const priceList = [500, 1000, 2000, 4000]
---
+++ utils/selectorLists.ts
@@ -0,0 +1,12 @@
+export const typesPKM: string[] = [
+    "Acier","Combat", "Dragon",
+    "Eau", "Electrik", "Fée", 
+    "Feu", "Glace", "Insecte", 
+    "Normal", "Plante", "Poison", 
+    "Psy", "Roche", "Sol", 
+    "Spectre", "Ténébre", "Vol"
+]
+
+export const rarityList: string[] = ["Commune", "Peu Commune", "Rare", "Légendaire"]
+
+export const priceList: number[] = [500, 1000, 2000, 4000]
