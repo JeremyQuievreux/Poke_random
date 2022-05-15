a0d9990ec13f364382a7ea22718d75a0aa713ad4
Quievreux <jquievreux.devpro@gmail.com>
2022-05-15 20:16:27 +0200

    new card style

--- comps/Card.tsx
+++ comps/Card.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/comps/Card.module.scss'
 
 import { setBGColor } from '../utils/BGColorFunction';
 
-interface IPokemon {
+type IPokemon = {
     _id: string;
     gen: number;
     dex_number: number;
@@ -28,24 +28,32 @@ const Card = ({card}:CardComponantProps) => {
     const type = card.type[0]
 
   return (
-    <div className={styles.card_container}>
-        <div className={styles.card_header} style={{backgroundColor: setBGColor(type)}}>
-            <p>N° {card.dex_number}</p>
-            <p>{card.name}</p>
+      <div className={styles.card_main_container}>
+        <div className={styles.card_container}>
+            <div className={styles.decoration_border}>
+                <div className={styles.card_header} style={{backgroundColor: setBGColor(type)}}>
+                    <p>N° {card.dex_number}</p>
+                    <p>{card.name}</p>
+                </div>
+                <div className={styles.card_body}>
+                    <img src={card.picURL} width={200} height={200} />
+                </div>
+                <div className={styles.card_typeline}>
+                    {card.type.map((type, index) => {
+                        return <p key={index} className={styles.type_square} style={{backgroundColor: setBGColor(type)}}>
+                                    {type}
+                                </p>
+                    })}
+                </div>
+                <div className={styles.card_footer} style={{backgroundColor: setBGColor(type)}}>
+                    <p>Taille : {card.height / 100} m </p>
+                    <p>Poids : {card.weight / 1000} Kg</p>
+                </div>
+            </div>
         </div>
-        <div className={styles.card_body}>
-            <img src={card.picURL} width={200} height={200} />
-        </div>
-        <div className={styles.card_typeline}>
-            {card.type.map((type, index) => {
-                return <p key={index} className={styles.type_square} style={{backgroundColor: setBGColor(type)}}>
-                            {type}
-                        </p>
-            })}
-        </div>
-        <div className={styles.card_footer} style={{backgroundColor: setBGColor(type)}}>
-            <p>Taille : {card.height / 100} m </p>
-            <p>Poids : {card.weight / 1000} Kg</p>
+        <div className={styles.buy_line}>
+            <p>{card.price} PkC</p>
+            <button>Acheter</button>
         </div>
     </div>
   )
