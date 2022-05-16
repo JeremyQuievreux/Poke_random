37c2b4f5f83d9b68fb764f8d9ee637854228ee17
Quievreux <jquievreux.devpro@gmail.com>
2022-05-16 16:18:38 +0200

    add style buy card modal

--- comps/BuyCardModal.tsx
+++ comps/BuyCardModal.tsx
@@ -16,6 +16,8 @@ const BuyCardModal = () => {
     const { userIsLog } = useContext(UserContext);
     const { setIsLogModalOpen } = useContext(ModalContext);
 
+    const haveMoney = buyCardModalInfos.userCoin >= buyCardModalInfos.cardPrice;
+
     const buycard = (cardID: string, userID: string) =>  {
         console.log("card id : " + cardID + " userID : " + userID);
         axios.post('/api/cards/buyCard',{cardID,userID})
@@ -25,36 +27,62 @@ const BuyCardModal = () => {
         })
     }
 
-    const haveMoney = buyCardModalInfos.userCoin >= buyCardModalInfos.cardPrice;
-
-  return (
-        <div className={styles.buyCardModal_externe} onClick={() => setShowBuyCardModal(false)}>
-            <div className={styles.buyCardModal_interne} onClick={(e) => e.stopPropagation()}>
-                {userIsLog ?
-                    <>
-                        <p>Buy Card Modal</p>
-                        <p>CardID : {buyCardModalInfos.cardID}</p>
-                        <p>CardName : {buyCardModalInfos.cardName}</p>
-                        <p>CardPrice : {buyCardModalInfos.cardPrice}</p>
-                        <p>UserID : {buyCardModalInfos.userID}</p>
-                        <p>UserCoin : {buyCardModalInfos.userCoin}</p>
-    
-                        <p>Voulez vous acheter cette la carte de {buyCardModalInfos.cardName} ?</p>
-                        <button onClick={() => {
-                            buycard(buyCardModalInfos.cardID, buyCardModalInfos.userID)
-                            setShowBuyCardModal(false)
-                        }
-                        }>OUI</button>
-                        <button onClick={() => setShowBuyCardModal(false)}>NON</button>
-                    </> :
-                    <>
+    const modalRender = () => {
+        if(userIsLog){
+            if (haveMoney) {
+                return (<>
+                        <div className={styles.modal_header}>
+                            <h2>Confirmation achat</h2>
+                        </div>
+                        <div className={styles.modal_body}>
+                            <p>Voulez vous acheter cette la carte de {buyCardModalInfos.cardName} ?</p>
+                        </div>
+                        <div className={styles.modal_footer}>
+                            <button onClick={() => {
+                                buycard(buyCardModalInfos.cardID, buyCardModalInfos.userID)
+                                setShowBuyCardModal(false)
+                            }
+                            }>OUI</button>
+                            <button onClick={() => setShowBuyCardModal(false)}>NON</button>
+                        </div>
+                    </>)
+            } else {
+                return (<>
+                        <div className={styles.modal_header}>
+                            <h2>Probleme budgétaire</h2>
+                        </div>
+                        <div className={styles.modal_body}>
+                            <p>Vous n'avez pas assez d'argent pour acheter cette carte</p>
+                        </div>
+                        <div className={styles.modal_footer}>
+                            <button onClick={() => setShowBuyCardModal(false)}>Fermer</button>
+                        </div>
+                    </>)
+            }
+        } else {
+            return (<>
+                    <div className={styles.modal_header}>
+                        <h2>Connection requise</h2>
+                    </div>
+                    <div className={styles.modal_body}>
                         <p>Vous devez être connecté pour acheter une carte</p>
+                    </div>
+                    <div className={styles.modal_footer}>
                         <button onClick={() => {
                             setShowBuyCardModal(false)
                             setIsLogModalOpen(true)
                         }}>Se Connecter</button>
-                    </>
-                }
+                    </div>
+                </>)
+        }
+    }
+
+
+  return (
+        <div className={styles.buyCardModal_externe} onClick={() => setShowBuyCardModal(false)}>
+            <div className={styles.buyCardModal_interne} onClick={(e) => e.stopPropagation()}>
+                <button className={styles.close_button} onClick={() => setShowBuyCardModal(false)}>X</button>
+                {modalRender()}
             </div>
         </div>
     )
