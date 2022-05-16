73d61173fb9f567ec527172bc8fce7dccf66d409
Quievreux <jquievreux.devpro@gmail.com>
2022-05-16 19:11:50 +0200

    update height of pages

---
+++ pages/collection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from "../styles/pages/Collection.module.scss"
+
+const Collection = () => {
+  return (
+    <div className={styles.collection_container}>
+        <h2>Collection Page</h2>
+    </div>
+  )
+}
+
+export default Collection
