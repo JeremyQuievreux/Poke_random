24bd05cfbae3c3116e61d128e11b541a09a596de
Quievreux <jquievreux.devpro@gmail.com>
2022-05-19 10:05:37 +0200

    refactoring collection useeffect fetch

---
+++ pages/api/users/getuserallcards.ts
@@ -0,0 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const jwt = require('jsonwebtoken');
+
+type DataType = {
+  error: boolean,
+  message: string,
+  data?: any
+}
+
+import dbConnect from '../../../utils/dbConnect'
+import UserModel from '../../../models/User'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<DataType>) {
+
+    const token = req.headers.authorization?.split(' ')[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, (err: any, decoded: any) => {
+        //token invalid
+        if (err) {
+            res.status(200).send({error: true, message: 'Invalid token'});
+        //token valid    
+        } else {
+            dbConnect()
+
+            UserModel.findById(decoded.id)
+                .populate({ path: 'cardsList.card', model: 'pokemon' })
+                .then((result) => {
+                    res.status(200).send({error: false, message: 'ok', data: result})
+                })
+                .catch((err) => {
+                    res.status(200).send({error: true, message: 'No user found'})
+                })
+            }
+    })
+}
--- pages/collection.tsx
+++ pages/collection.tsx
@@ -1,11 +1,9 @@
-import React,{ useContext, useEffect, useState } from 'react'
+import React,{ useEffect, useState } from 'react'
 
 import styles from "../styles/pages/Collection.module.scss"
 
-import { UserContext } from '../context/UserContext'
 import axios from 'axios'
 import Card from '../comps/Card';
-import BuyBtn from '../comps/BuyBtn';
 
 interface PokemonType {
   _id: string;
@@ -29,40 +27,24 @@ type CardType = {
 
 const Collection = () => {
 
-  const { userInfos } = useContext(UserContext)
-  const [ userCardsList, setUserCardsList ] = useState<[CardType]>()
+  const [ userCardsList, setUserCardsList ] = useState<[CardType]|null>(null)
 
-  
-
-  const getUserInfos = (IDtoken: string|null) => {
-    axios.get('/api/users/getUserInfos', {
+  const getUserAllCards = (localToken:string) => {
+    axios.get('/api/users/getuserallcards', {
       headers: {
-        'Authorization': `Bearer ${IDtoken}`
+        'Authorization': `Bearer ${localToken}`
       }
     })
-    .then(res => {
-      if(res.data.error === true){
-        console.log("error with token");
-      } else {
-        getallusercards(res.data.data._id)
-      }
-    })
-  }
-
-  const getallusercards = (userID:string) => {
-    axios.get(`/api/users/getusercards`, {
-      params: {
-        userID
-      }
-    })
-    .then(res => {
-      setUserCardsList(res.data.data.cardsList)
+    .then((result)=> {
+      setUserCardsList(result.data.data.cardsList)
     })
   }
 
   useEffect(() => {
     const localToken:string|null = localStorage.getItem('@pkm-cnc')
-    getUserInfos(localToken)
+    if(localToken){
+      getUserAllCards(localToken)
+    }
   },[])
   
     
@@ -70,14 +52,22 @@ const Collection = () => {
     <div className={styles.collection_container}>
         <h2>Collection Page</h2>
         <div className={styles.cards_container}>
-          {userCardsList?.map((cardAndQuantity, index) => {
-            return (
-            <div className={styles.sub_container} key={index}>
-              <Card card={cardAndQuantity.card}/>
-              <p>{cardAndQuantity.quantity}</p>
-            </div>
-              )
-          })}
+          {userCardsList ?
+            userCardsList.length >= 1 
+              ?
+              userCardsList?.map((cardAndQuantity, index) => {
+                return (
+                <div className={styles.sub_container} key={index}>
+                  <Card card={cardAndQuantity.card}/>
+                  <p>{cardAndQuantity.quantity}</p>
+                </div>
+                )
+              })
+              : 
+              <p>You have no cards in your collection</p>
+            :
+            <p>Loading ...</p>
+          }
         </div>
     </div>
   )
