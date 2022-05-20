a8d2eb2718fd9adfa1653760fb96191bc10918c4
Quievreux <jquievreux.devpro@gmail.com>
2022-05-20 15:33:29 +0200

    update collection with radio btn

--- pages/collection.tsx
+++ pages/collection.tsx
@@ -32,13 +32,9 @@ type CardType = {
 const Collection = () => {
 
   const { userCardsList, setUserCardsList , refreshUserCollection} = useContext(SellCardModalContext)
+  const [ whatToSee, setWhatToSee ] = useState<string>("all")
 
-  useEffect(() => {
-    const localToken = localStorage.getItem('@pkm-cnc')
-    if(localToken){
-      refreshUserCollection(localToken)
-    }
-  },[])
+  const [cardsToSee, setCardsToSee] = useState<CardType[]>([])
 
   const setFilter = (quantity:number) => {
     if(quantity === 0){
@@ -46,15 +42,58 @@ const Collection = () => {
     }
   }
   
+  const handleRadioChange = (e:any) => {
+    setWhatToSee(e.target.value)
+  }
+
+  useEffect(() => {
+    const localToken = localStorage.getItem('@pkm-cnc')
+    if(localToken){
+      axios.get('/api/users/getuserallcards', {
+        headers: {
+          'Authorization': `Bearer ${localToken}`
+        }
+      })
+      .then((result)=> {
+        setUserCardsList(result.data.data.cardsList)
+        if(userCardsList){
+          setCardsToSee(userCardsList)
+        }
+      })
+    }
+  },[])
+
+
+  useEffect(()=> {
+    if(userCardsList){
+      if (whatToSee === "all") {
+        setCardsToSee(userCardsList)
+      }
+      if (whatToSee === "mine") {
+        setCardsToSee(userCardsList.filter(card => card.quantity > 0))
+      }
+      if (whatToSee === "duplicate") {
+        setCardsToSee(userCardsList.filter(card => card.quantity > 1))
+      }
+    }
+  },[whatToSee, userCardsList])
+
     
   return (
     <div className={styles.collection_container}>
-        <h2>Collection Page</h2>
+        <div className={styles.radio_btn_block} onChange={(e) => handleRadioChange(e)}>
+          <input type="radio" name="seeing" id="all" value="all" defaultChecked={whatToSee === "all"}/>
+          <label> Toutes les cartes </label>
+          <input type="radio" name="seeing" id="mine" value="mine" defaultChecked={whatToSee === "mine"}/>
+          <label> Mes cartes </label>
+          <input type="radio" name="seeing" id="duplicate" value="duplicate" defaultChecked={whatToSee === "duplicate"}/>
+          <label> Mes doubles </label>
+        </div>
         <div className={styles.cards_container}>
-          {userCardsList ?
-            userCardsList.length >= 1 
+          {cardsToSee ?
+            cardsToSee.length >= 1 
               ?
-              userCardsList?.map((cardAndQuantity, index) => {
+              cardsToSee?.map((cardAndQuantity, index) => {
                 return (
                 <div className={styles.sub_container} key={index}>
                   <div style={setFilter(cardAndQuantity.quantity)}>
@@ -70,7 +109,7 @@ const Collection = () => {
                 )
               })
               : 
-              <p>You have no cards in your collection</p>
+              <p>Pas de cartes trouvées</p>
             :
             <p>Loading ...</p>
           }
