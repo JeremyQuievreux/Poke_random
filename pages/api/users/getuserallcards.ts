10a9348173eb78068a967c50016d0da49a37f62e
Quievreux <jquievreux.devpro@gmail.com>
2022-05-20 23:33:05 +0200

    maybe fixed

--- pages/api/users/getuserallcards.ts
+++ pages/api/users/getuserallcards.ts
@@ -13,17 +13,17 @@ import dbConnect from '../../../utils/dbConnect'
 import UserModel from '../../../models/User'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<DataType>) {
-    dbConnect()
-
+    
     const token = req.headers.authorization?.split(' ')[1];
-
+    
     jwt.verify(token, process.env.JWT_SECRET, (err: any, decoded: any) => {
         //token invalid
         if (err) {
             res.status(200).send({error: true, message: 'Invalid token'});
-        //token valid    
+            //token valid    
         } else {
-
+            dbConnect()
+            
             UserModel.findById(decoded.id)
                 .populate({ path: 'cardsList.card', model: 'pokemon' })
                 .then((result) => {
